d1d6414d5f7d22c2c809fc120b80dcb2f0bd63a2
helderdiin <helderdiin.work@gmail.com>
2020-07-06 22:11:20 -0300

    Deixando o header dinâmico

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,24 +1,74 @@
 import React from 'react';
-import { FaSearch } from 'react-icons/fa';
+import { FaSearch, FaArrowLeft } from 'react-icons/fa';
+import { useRouteMatch } from 'react-router-dom';
 
 import './styles.css';
 
-function Header() {
+import Switch from '../Switch';
+import { currency } from '../../services/format';
+
+function Header({ product }) {
+  const match = useRouteMatch({
+    path: '/product/:productId',
+  });
+
+  const getHeaderTitleElement = () => {
+    if (product) {
+      return (
+        <div className="header__title">
+          <span>
+            {product.nome}
+          </span>
+          <div className="header__title-price">
+            {currency(product.valor)}
+          </div>
+          <div className="header__title-favorite">
+            <Switch />
+            <span className="header__switch-text">tornar favorito</span>
+          </div>
+        </div>
+      );
+    }
+
+    return (
+      <div className="header__title">
+        <span>Empresa XPTO </span>
+        &nbsp;- Conheça todos os nossos produtos
+      </div>
+    );
+  };
+
+  const getHeaderSubtitleElement = () => {
+    if (product) {
+      return <span>{product.decricaoCurta}</span>;
+    }
+
+    return <span>Listagem de produtos - clique no produto desejado para saber mais</span>;
+  };
+
   return (
     <header className="header">
       <div className="header__description">
-        <div className="header__title">
-          <span>Empresa XPTO </span>
-          - Conheça todos os nossos produtos
+        {getHeaderTitleElement()}
+        <div className="header__subtitle">
+          {getHeaderSubtitleElement()}
         </div>
-        <div className="header__subtitle">Listagem de produtos - clique no produto desejado para saber mais</div>
       </div>
       <div className="header__search">
-        <input placeholder="Buscar" />
-        <span className="header__icon">
-          <FaSearch />
-          <div />
-        </span>
+        {match
+          ? (
+            <div className="header__back-button">
+              <FaArrowLeft />
+            </div>
+          )
+          : ''}
+        <div className="header__input">
+          <input placeholder="Buscar" />
+          <span className="header__icon">
+            <FaSearch />
+            <div />
+          </span>
+        </div>
       </div>
     </header>
   );
