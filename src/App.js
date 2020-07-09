dd04e82e042517ee814a157f78f0829a3d34fa2b
helderdiin <helderdiin.work@gmail.com>
2020-07-09 19:41:43 -0300

    Instalando react-intl & Adicionando i18n messages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { IntlProvider } from 'react-intl';
 
 import './global.css';
 
 import Routes from './routes';
 import store from './store';
+import messages from './i18n';
 
 import Menu from './components/Menu';
 
 function App() {
   return (
     <Provider store={store}>
-      <BrowserRouter>
-        <Menu />
+      <IntlProvider locale="pt-BR" messages={messages['pt-BR']}>
+        <BrowserRouter>
+          <Menu />
 
-        <div id="app">
-          <main>
-            <Routes />
-          </main>
-        </div>
-      </BrowserRouter>
+          <div id="app">
+            <main>
+              <Routes />
+            </main>
+          </div>
+        </BrowserRouter>
+      </IntlProvider>
     </Provider>
   );
 }
---
+++ src/i18n/index.js
@@ -0,0 +1,7 @@
+import ptBr from './messages/pt-br';
+import enUs from './messages/en-us';
+
+export default {
+  'pt-BR': ptBr,
+  'en-US': enUs,
+};
